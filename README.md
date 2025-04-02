ca329d77af445a97ed770aef461def36b83995bb
Sourav61 <sourav61pahwa@gmail.com>
2025-04-03 00:09:36 +0530

    Feat: Update README, add base functionality to show current time on clock

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Digital-Clock
+
+Tasklist
+
+- Add the light/dark mode on the basis of the current time i.e. AM/PM of the current timezone.
+- 
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let minutes = currentTime.getMinutes();
 
 let seconds = currentTime.getSeconds();
 
-console.log('The time info is: hour = ' + hour + ' minutes = ' + minutes + ' seconds = ' + seconds);
+console.log('The time info is: hour = ' + hour + ', minutes = ' + minutes + ', seconds = ' + seconds);
 
 // Total degrees = 360 deg, Total hours = 12 hrs, Rotation per hour = 30 deg;
 // To Calculate the amount of rotation needed for the hour hand,
@@ -17,8 +17,26 @@ console.log('The time info is: hour = ' + hour + ' minutes = ' + minutes + ' sec
 
 let hourAngle = hour * 30;
 
-// Total degrees = 360 deg, Total minutes = (12 * 60) minutes, Rotation per minute = 0.5 deg;
+// Total degrees = 360 deg, Total minutes = 60 minutes, Rotation per minute = 6 deg;
 // To Calculate the amount of rotation needed for the minute hand,
-// multiply the current time(minutes) by 30 degrees
+// multiply the current time(minutes) by 6 degrees
 
-// let minutes = minutes * 0.5;
+let minutesAngle = minutes * 6;
+
+// Total degrees = 360 deg, Total minutes = 60 minutes, Rotation per minute = 6 deg;
+// To Calculate the amount of rotation needed for the minute hand,
+// multiply the current time(minutes) by 6 degrees
+
+let secondsAngle = seconds * 6;
+
+let hourHandDiv = document.querySelector('.hour-hand');
+
+hourHandDiv.style.transform = `rotate(${hourAngle}deg)`;
+
+let minutesHandDiv = document.querySelector('.minutes-hand');
+
+minutesHandDiv.style.transform = `rotate(${minutesAngle}deg)`;
+
+let secondsHandDiv = document.querySelector('.seconds-hand');
+
+secondsHandDiv.style.transform = `rotate(${secondsAngle}deg)`;
